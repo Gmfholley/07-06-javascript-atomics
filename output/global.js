4e00e903ae757c58f378de38a1af54e2cd372f72
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-06 11:02:21 -0500

    Creates the first of my javascript lessons
    Finished lesson 1 & 2

---
+++ output/global.js
@@ -0,0 +1,13 @@
+var my_name = "Wendy";
+
+alert("Hello!  My name is " + my_name);
+
+console.log("My favorite breakfast is coffee and waffles.");
+console.log("I have two cats.");
+console.log("I love pasta and pizza, so Valentino's is a favorite.");
+
+console.log("I take 1-2 naps per week.");
+
+console.log("I'd love to take a nap three to four times a week.");
+
+alert("Goodbye!  Thanks for visiting.")
