3184ccfd1b916bd611acb6264e3dc71a2c17e667
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-06 12:23:01 -0500

    Lesson four!

---
+++ user-input/global.js
@@ -0,0 +1,17 @@
+var name = prompt("What is your name?");
+var age = prompt("How old are you?");
+var hometown = prompt("Where are you from?");
+var fav_num = prompt("What is your favorite number?");
+
+
+console.log("Your name is " + name + ".");
+console.log("Your age is " + age + ".");
+console.log("Your hometown is " + hometown + ".");
+console.log("Your favorite number is " + fav_num + ".");
+
+alert("Your name is " + name + ".");
+alert("Your age is " + age + ".");
+alert("Your hometown is " + hometown + ".");
+alert("Your favorite number is " + fav_num + ".");
+
+console.log(typeof(fav_num));
