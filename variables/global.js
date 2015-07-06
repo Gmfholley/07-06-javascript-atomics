3603a343fd2170717acff2891086664067722782
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-06 11:18:23 -0500

    Lesson three!

---
+++ variables/global.js
@@ -0,0 +1,14 @@
+var name = "Patricia";
+var age = "30";
+var hairColor = "brown";
+
+
+alert("My name is " + name + ".");
+alert("My age is " + age + ".");
+alert("My hair color is " + pieces + ".");
+
+
+
+console.log("My name is " + name + ".");
+console.log("My age is " + age + ".");
+console.log("My hair color is " + pieces + ".");
